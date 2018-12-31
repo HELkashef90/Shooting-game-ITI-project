4ffd972a0c99b99e5baa7413f394f8f36a4b4886
HELkashef90 <h.elkashef90@gmail.com>
2018-12-31 23:03:01 +0200

    char-level-page, bk game audio

---
+++ Level-char-popup.js
@@ -0,0 +1,6 @@
+//close gamePopup 
+var gamePupup = document.getElementById("gamePupUp");
+var closBtn4 = document.getElementById('close4');
+function closePupUp4()
+{gamePupup.style.display = "none";}
+closBtn4.addEventListener('click',closePupUp4);
