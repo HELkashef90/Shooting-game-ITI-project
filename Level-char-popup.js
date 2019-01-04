b6a3cdd4103b61dc439a5d52bfe7a5289e2a1abc
HELkashef90 <h.elkashef90@gmail.com>
2019-01-04 02:47:48 +0200

    organize some files & change all forst pgs bk audio&usrInfo div opcty .7

--- Level-char-popup.js
+++
@@ -1,6 +0,0 @@
-//close gamePopup 
-var gamePupup = document.getElementById("gamePupUp");
-var closBtn4 = document.getElementById('close4');
-function closePupUp4()
-{gamePupup.style.display = "none";}
-closBtn4.addEventListener('click',closePupUp4);
